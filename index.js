ffd6c8e1804ba3c530b4e1bf1bf80ae4bd5714f0
chaokinhong <3484715033@qq.com>
2022-06-23 01:31:54 +0800

    make it new]

--- index.js
+++ index.js
@@ -19,12 +19,13 @@ app.get("/", (req, res) => {
 
 //mongodb
 const CONNECTION_URL = process.env.CONNECTION_URL;
+const HOST = '0.0.0.0';
 const PORT = process.env.PORT || 5000;
 
 //connect to databse
 mongoose
   .connect(CONNECTION_URL)
   .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+    app.listen(PORT, HOST,() => console.log(`Server running on port: ${PORT}`))
   )
   .catch((error) => console.log(error.message));
