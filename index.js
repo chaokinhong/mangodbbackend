de588aa549adfb67ccf42ecf0924540037660bd4
chaokinhong <3484715033@qq.com>
2022-06-23 02:36:29 +0800

    'hope'

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from "dotenv";
 import postRoutes from "./routes/post.js";
 
 const app = express();
-dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -19,12 +17,11 @@ app.get("/", (req, res) => {
 
 //mongodb
 const CONNECTION_URL = process.env.CONNECTION_URL;
-const PORT = process.env.PORT  || 5000 ;
 
 //connect to databse
 mongoose
   .connect(CONNECTION_URL)
   .then(() =>
-    app.listen(PORT,() => console.log(`Server running on port: ${PORT}`))
+    app.listen( process.env.PORT || 5000,() => console.log(`Server running on port: ${PORT}`))
   )
   .catch((error) => console.log(error.message));
